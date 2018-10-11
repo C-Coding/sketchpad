e34e9819fd8e15dc288e7a0a4bb3f6afe537e712
Carbon <c_coding@foxmail.com>
2018-10-11 22:19:58 +0800

    temp

--- test/test.js
+++ test/test.js
@@ -7,14 +7,17 @@ module.exports = {
     browser
       .url('http://localhost:8080')
       .waitForElementVisible('body', 1000)
-      browser.expect.element('#main').to.not.be.present
+    // browser.expect.element('#main').to.not.be.present
 
-      // .setValue('#kw', 'nightwatch')
-      // .click('#su')
-      // .pause(3000)
-      // .waitForElementVisible('#content_left', 3000)
-      // .end();
 
-      browser.end();
+    
+
+    // .setValue('#kw', 'nightwatch')
+    // .click('#su')
+    // .pause(3000)
+    // .waitForElementVisible('#content_left', 3000)
+    // .end();
+
+    browser.end();
   }
 };
